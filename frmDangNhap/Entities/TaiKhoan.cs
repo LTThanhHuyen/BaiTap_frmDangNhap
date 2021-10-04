5342195f5664a637931195d4cca753fce83a00ab
LTThanhHuyen <lethithanhhuyen2000@gmail.com>
2021-10-04 15:27:38 +0700

    first commit

---
+++ frmDangNhap/Entities/TaiKhoan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frmDangNhap.Entities
+{
+    public class TaiKhoan
+    {
+        public string TenDangNhap { get; set; }
+        public string MatKhau { get; set; }
+        public static List<TaiKhoan> GetList()
+        {
+            var ls = new List<TaiKhoan>();
+            ls.Add(new TaiKhoan()
+            {
+                TenDangNhap = "ndungithue",
+                MatKhau = "Abc@123"
+            });
+            return ls;
+        }
+
+        public static TaiKhoan Get(string tendn, string mk)
+        {
+            var dbTaiKhoan = GetList();
+            var taikhoan = dbTaiKhoan.Where(s => s.TenDangNhap == tendn && s.MatKhau == mk).FirstOrDefault();
+            return taikhoan;
+
+        }
+    }
+}
---
+++ frmDangNhap/frmDangNhap.cs
@@ -0,0 +1,43 @@
+﻿using frmDangNhap.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmDangNhap
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var tendn = textBox1.Text;
+            var mk = textBox2.Text;
+            TaiKhoan
+                taikhoan;
+            taikhoan = TaiKhoan.Get(tendn, mk);
+
+            if (taikhoan == null)
+            {
+                MessageBox.Show($"Đăng nhập thất bại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                MessageBox.Show($"Đăng nhập thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+        }
+
+    }
+}
